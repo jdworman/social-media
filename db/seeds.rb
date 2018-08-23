fef4dc4ad0165f25f82bd9366cd1f718559f7bc1
Joseph Worman <jdworman84@gmail.com>
2018-08-23 10:13:30 -0400

    debugged

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,13 +13,13 @@ end
 
 User.all.each do |m|
 20.times do
-  posts = Post.new(
+  messages = Post.new(
   firstname: Faker::Name.first_name,
   lastname: Faker::Name.last_name,
   title: Faker::WorldCup.city,
   content: Faker::BackToTheFuture.quote,
   user_id: m.id
 )
-    posts.save
+    messages.save
   end
 end
